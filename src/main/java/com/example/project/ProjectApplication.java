023d3e762c973706ff4a851f93a73ae938993de5
Your Name <you@example.com>
2024-03-28 12:00:01 +0200

    creation of my student API

---
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -0,0 +1,20 @@
+package com.example.project;
+
+import com.example.project.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/project/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.project.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student irakiza = new Student(
+                    "IRAKIZA",
+                    "irakiza44@gmail.com",
+                    LocalDate.of(1997, Month.DECEMBER, 20)
+            );
+
+            Student ineza = new Student(
+                    "INEZA",
+                    "ineza44@gmail.com",
+                    LocalDate.of(2007, Month.OCTOBER, 25)
+            );
+
+            repository.saveAll(List.of(irakiza, ineza));
+        };
+    }
+}
